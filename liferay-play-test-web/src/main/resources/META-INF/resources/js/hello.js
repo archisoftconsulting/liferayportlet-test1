44dca187ec0343fcdf76fc9b6b0628e47cdc8bba
archisoftconsulting <github@archisoftconsulting.com>
2017-04-03 16:39:01 +0800

    Update hello.js

--- liferay-play-test-web/src/main/resources/META-INF/resources/js/hello.js
+++ liferay-play-test-web/src/main/resources/META-INF/resources/js/hello.js
@@ -2,7 +2,9 @@ angular.module('demo', [])
 .controller('Hello', function($scope, $http) {
     $http.get('http://localhost/find').
         then(function(response) {
-        alert(response.data + '|' + response);
-            $scope.users = response;
+        console.log(response);
+       
+            $scope.users = response.data;
+        
         });
 });
